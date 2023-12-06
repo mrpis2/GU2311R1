4b3b76d7d8d218757cf8079e7c0d22d0c885ea21
mrpis2 <warhammerprogamer@gmail.com>
2023-12-06 21:14:00 +0700

    Hien thi thoi gian hien tai

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Systemtime
+{
+ class Program
+{
+    private static void Main(string[] args) 
+    {
+        DateTime localTime = DateTime.Now;
+        Console.WriteLine("Thoi gian hien tai la: " + localTime);
+    }
+}
+}
+ 
